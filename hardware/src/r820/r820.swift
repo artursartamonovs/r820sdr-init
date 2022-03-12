ec61b3e0e6084950eb4d5c95f83e442a01765a35
Arturs Artamonovs <arturs.artamonovs@protonmail.com>
2022-03-12 17:48:54 +0000

    Working project

---
+++ hardware/src/r820/r820.swift
@@ -0,0 +1,16 @@
+//
+//  r820.swift
+//  r820sdr
+//
+//  Created by Jacky Jack on 10/03/2022.
+//
+
+import Foundation
+
+
+final class r820sdr {
+    //let deviceCounr = rtlsdr_get_device_count()
+    //r820sdr.rtlsdr_get_device_count()
+    //libusb_open(1, 1);
+    //r82xx
+}
--- r820sdr/main.swift
+++ r820sdr/main.swift
@@ -7,5 +7,22 @@
 
 import Foundation
 
+
+
+
 print("Hello, World!")
 
+//var dev:UnsafePointer<rtlsdr_dev> = 0
+var devPtr: UnsafePointer<OpaquePointer?>
+var dev:OpaquePointer?
+let count = rtlsdr_get_device_count()
+
+if count <= 0 {
+    print("All gona blow up there is no dongles")
+}
+
+rtlsdr_open(&dev, 0)
+
+rtlsdr_close(dev)
+
+print("Number of devices \(count)")
